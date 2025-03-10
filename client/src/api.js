ab9bb782966f569e579bcccccb72e1d4de1095b6
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-03-10 20:09:07 +0500

    10.03.2025 5443

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "api.kulzhabay.kz",
+    baseURL: "https://api.kulzhabay.kz",
     timeout: 1000 * 30,
     headers: {
         "X-Requested-With": "XMLHttpRequest",
