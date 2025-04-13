f02fd3f5459f557f00f63b7bd47cd3e4fcea3069
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-04-13 19:13:21 +0500

    13.04.2025 1

---
+++ getUEE.js
@@ -0,0 +1,28 @@
+const EventHistory = require('./models/EventHistory.js');
+
+async function getUniqueEmailEvents() {
+  try {
+    const uniqueEvents = await EventHistory.aggregate([
+      {
+        $match: {
+          eventType: "subscription.recurring.payment.success"
+        }
+      },
+      {
+        $group: {
+          _id: "$rawData.buyer.email",
+          latestEvent: { $last: "$$ROOT" }
+        }
+      },
+      {
+        $replaceRoot: { newRoot: "$latestEvent" }
+      }
+    ]);
+    
+    console.log('Уникальные события:', uniqueEvents);
+    return uniqueEvents;
+  } catch (err) {
+    console.error('Ошибка при получении событий:', err);
+    throw err;
+  }
+}
