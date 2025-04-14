ba61db22b5582baa4840375a030de51e5fe6ff75
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-04-14 20:01:59 +0500

    14.03.2025 1234567

--- client/src/Pages/Pay.js
+++ client/src/Pages/Pay.js
@@ -106,7 +106,7 @@ export default function Pay() {
     };
 
     return <div className="flex items-center justify-center bg-black min-h-screen text-center">
-        <div className="text-2xl text-white">В данный момент набор в Академию закрыт.</div>
+        <div className="text-2xl text-white text-center">В данный момент набор в Академию закрыт.</div>
     </div>
     
 
