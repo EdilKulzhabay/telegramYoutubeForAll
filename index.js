3ff87de69970ae0b80201cdf08fe08ff2a9ed43f
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-04-27 23:55:20 +0500

    28.04.2025 1

--- index.js
+++ index.js
@@ -1345,6 +1345,16 @@ app.post("/cancelAllSubscriptions", async (req, res) => {
   }
 });
 
+app.post("/testTomoru", async (req, res) => {
+  try {
+    console.log("we in testTomoru");
+    console.log("req.body = ", req.body);
+
+    res.status(200).json({success: true});
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка сервера", error });
+  }
+})
 const PORT = process.env.PORT || 3006;
 app.listen(PORT, async () => {
   console.log(`Сервер запущен на порту ${PORT}`);
