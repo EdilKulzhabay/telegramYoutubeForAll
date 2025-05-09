ec457a78c2a18b4ffb46f434099d1513960885be
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-05-10 02:44:16 +0500

    10.05.2025 1

--- index.js
+++ index.js
@@ -1344,18 +1344,3 @@ app.post("/cancelAllSubscriptions", async (req, res) => {
     res.status(500).json({ error: 'Внутренняя ошибка сервера' });
   }
 });
-
-app.post("/testTomoru", async (req, res) => {
-  try {
-    console.log("we in testTomoru");
-    console.log("req.body = ", req.body);
-
-    res.status(200).json({success: true});
-  } catch (error) {
-    res.status(500).json({ message: "Ошибка сервера", error });
-  }
-})
-const PORT = process.env.PORT || 3006;
-app.listen(PORT, async () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
-});
